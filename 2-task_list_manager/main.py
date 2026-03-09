fd35f5d7bb6d52a35985d4165f354da8d86ee293
Anette W <wralsen@gmail.com>
2026-03-09 17:13:31 +0100

    Added setup for exercise 2.

---
+++ 2-task_list_manager/main.py
@@ -0,0 +1,8 @@
+# 2. Task List Manager (with separate module)
+# Create a Python module named tasks.py that contains two functions:
+# * add_task(task_list, task)
+# * remove_task(task_list, task)
+# In your main script, import tasks.py.
+# Start with an empty list, repeatedly ask the user for input: "add " or "remove ".
+# Use the imported functions to modify the list, and print the updated list after each operation.
+# Exit when the user types "done".
