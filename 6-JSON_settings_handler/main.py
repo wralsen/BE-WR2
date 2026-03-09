12a1ca7564d624768169f9c7fe533899b86314d4
Anette W <wralsen@gmail.com>
2026-03-09 20:27:19 +0100

    Updated solution to exercise 6 (JSON Settings Handler)

--- 6-JSON_settings_handler/main.py
+++ 6-JSON_settings_handler/main.py
@@ -5,21 +5,53 @@
 #  Handle any file read/write exceptions gracefully.
 import os, sys, json
 
-# read file
+def print_current_settings(settings):
+  print("\nCurrent settings:")
+  n = 1
+  for key in settings:
+    print(n,key,":",settings[key])
+    n += 1
+
+# welcome message
+print("~~ Welcome to the JSON Settings Handler ~~")
+
 filename = "settings.json"
-# Comment out the next line when not running from VSC (VSC requires path from working directory, not script directory)
-filename = "innleveringer/BE_WR2/BE-WR2/6-JSON_settings_handler/"+filename
+# Comment out the next line when not running from my VSC setup 
+filename = "innleveringer/BE_WR2/BE-WR2/6-JSON_settings_handler/"+filename # VSC requires path from working directory, not script directory
 
 try:
+  # read file
   io = open(filename, 'r')
   settings = json.load(io)
   io.close()
 
-  # printing current settings
-  print("Current settings:")
-  for key in settings:
-    print(key,":",settings[key])
+  print_current_settings(settings)
 
-except (FileNotFoundError):
-  print(f"Could not find file {filename}.")
+  print("To change a setting, please enter its number.") 
+  print("To exit, type 0.")
+  # using numbers instead of keys complicates the code but helps me learn more about dictionaries in Python
+  
+  setting_to_change = -1
+  while not (setting_to_change == 0):
+    try:
+      setting_to_change = int(input())
+      if (setting_to_change > len(settings)): # setting does not exist
+        raise ValueError
+      elif (setting_to_change == 0): # user wants to quit
+        print("Thank you for using the JSON Settings Handler.")
+        # update file
+        io = open(filename, 'w')
+        json.dump(settings, io)
+        io.close()
+      else: # user has typed a valid integer
+        setting_key = list(settings)[setting_to_change-1] # find key
+        print(f"You want to change the {setting_key}.")
+        new_value = input("Please type the new value:\n")
+        settings[setting_key] = new_value # update setting
+        print("Setting is updated.")
+        print_current_settings(settings)
+    except (TypeError, ValueError):
+      setting_to_change = input("You did not type a valid integer. Please try again.\n")
+except FileNotFoundError:
+  print("The setting file could not be found.")
 
